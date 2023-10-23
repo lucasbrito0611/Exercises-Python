d64128d8897034a75249c90f9b9b54e2ad18962e
lucasbrito0611 <122239789+lucasbrito0611@users.noreply.github.com>
2023-10-23 14:12:36 +0000

    23/10/2023 11:12

--- Listas POO/ListaPOO 9C/index.py
+++ Listas POO/ListaPOO 9C/index.py
@@ -1,14 +1,26 @@
 import streamlit as st
-from clienteUI import ClienteUI
-from servicoUI import ServicoUI
-from agendaUI import AgendaUI
+from templates.clienteUI import ClienteUI
+from templates.servicoUI import ServicoUI
+from templates.agendaUI import AgendaUI
 from abriragendaUI import AbrirAgendaUI
 
 class IndexUI:
-    def main():
-        # ClienteUI.Main()
-        # ServicoUI.Main()
-        # AgendaUI.Main()   
-        AbrirAgendaUI.Main()  
+    def sidebar():
+        op = st.sidebar.selectbox("Menu", ["Manter Clientes", "Manter Serviços",
+                                        "Manter Agenda", "Abrir Agenda do Dia",
+                                        "Confirmar Agendamento"])
+        if op == "Manter Clientes": st.session_state["page"] = "ManterClienteUI"
+        if op == "Manter Serviços": st.session_state["page"] = "ServicoUI"
+        if op == "Manter Agenda": st.session_state["page"] = "AgendaUI"
+        if op == "Abrir Agenda do Dia": st.session_state["page"] = "AbrirAgendaUI"
 
-IndexUI.main()        
+
+    def main():
+        IndexUI.sidebar()
+        if "page" not in st.session_state: st.session_state["page"] = "manter_clienteUI"
+        if st.session_state["page"] == "ClienteUI": ClienteUI.Main()
+        if st.session_state["page"] == "ServicoUI": ServicoUI.Main()
+        if st.session_state["page"] == "AgendaUI": AgendaUI.Main()
+        if st.session_state["page"] == "AbrirAgendaUI": AbrirAgendaUI.Main()
+
+IndexUI.main()
